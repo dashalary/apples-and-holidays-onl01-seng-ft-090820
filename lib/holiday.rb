564a0065635f2d91e291a4da738943c20f3bb586
Dasha Lary <dk2800@nyu.edu>
2020-09-14 16:31:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-if holiday_hash
+if holiday_hash.value
 end
 
 
